9d73fb42596437d3c79c738c6fed767fe627f611
beth <beth_menzies@flutterint.com>
2021-08-25 09:55:50 +0100

    aligning is/is not logic to existing isp tests
    makes the report more readable

--- StopwatchUITestsGherkin/StepsAndPages/steps.swift
+++ StopwatchUITestsGherkin/StepsAndPages/steps.swift
@@ -6,8 +6,8 @@ import Nimble
 class NavigationSteps: StepDefiner {
     override func defineSteps() {
 
-        step("the (?<button>Start|Stop|Reset|Lap) button (?<test>is|is not) displayed") { (match: StepMatches<String>) in
-            switch match["test"] {
+        step("the (?<button>Start|Stop|Reset|Lap) button (.*) displayed") { (match: StepMatches<String>) in
+            switch match[1] {
             case "is": expect(checkButton(button: match["button"]!).exists).to(beTrue(), description: "The \(match["button"]!) button does not exist")
             case "is not": expect(checkButton(button: match["button"]!).exists).to(beFalse(), description: "The \(match["button"]!) button does exist")
             default: XCTFail("Incorrect word entered in step, should be 'is' or 'is not'")
